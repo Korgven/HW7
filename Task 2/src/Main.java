0473128633af023994e638fd902d0fffb125f1d9
Korgven <kor_gven@mail.ru>
2026-01-22 20:23:12 +0300

    solving Task2 using While operator

--- Task 2/src/Main.java
+++ Task 2/src/Main.java
@@ -2,10 +2,17 @@ public class Main {
     public static void main(String[] args) {
         //todo Тут увольняем айтишников
         int countITemployee = 100;
-        for (int i = 1; i <= countITemployee; i++) {
+//        for (int i = 1; i <= countITemployee; i++) {
+//            if (i % 2 == 0) {
+//                System.out.println("An IT specialist with an ID " + i + " has been dismissed");
+//            }
+//        }
+        int i = 1;
+        while (i <= countITemployee) {
             if (i % 2 == 0) {
                 System.out.println("An IT specialist with an ID " + i + " has been dismissed");
             }
+            i++;
         }
     }
 }
